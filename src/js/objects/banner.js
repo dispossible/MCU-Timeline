df6533cc040377a77c372fda3b1aefdf68337b92
dispossible <mail@dispossible.com>
2017-07-14 23:02:54 +0100

    Reworked banner design

--- src/js/objects/banner.js
+++ src/js/objects/banner.js
@@ -18,26 +18,11 @@ class Banner {
     //Preload all needed images
     preload(){
 
-        //TODO: Only load the assest needed for the current resolution
         let files = [
-            "img/ban-captain.png",
-            "img/ban-groot.png",
-            "img/ban-hulk.png",
-            "img/ban-ironMan.png",
-            "img/ban-panther.png",
-            "img/ban-strange.png",
-            "img/ban-thor.png",
-            "img/ban-vision.png",
-            "img/ban-jet.png",
-            "img/ban-rubble1.png",
-            "img/ban-rubble2L.png",
-            "img/ban-rubble2R.png",
-            "img/ban-rubble3.png",
-            "img/ban-rubble4.png",
-            "img/ban-rubble4L.png",
-            "img/ban-rubble4R.png",
-            "img/ban-sky.png",
-            "img/ban-sky2.png",
+            "img/banner1.png",
+            "img/banner2.png",
+            "img/banner3.png",
+            "img/banner4.png",
             "img/ban-smoke1.png",
             "img/ban-smoke2.png",
         ];
@@ -63,18 +48,8 @@ class Banner {
         this.canvas = [
             d.createElement("canvas"), // 0  Screen
             d.createElement("canvas"), // 1  Fire
-            d.createElement("canvas"), // 2  Char1
-            d.createElement("canvas"), // 3  Char2
-            d.createElement("canvas"), // 4  Smoke
-            d.createElement("canvas"), // 5  Char3
-            d.createElement("canvas"), // 6  Rubble1
-            d.createElement("canvas"), // 7  Char4
-            d.createElement("canvas"), // 8  Smoke
-            d.createElement("canvas"), // 9  Rubble2
-            d.createElement("canvas"), // 10 Rubble3
-            d.createElement("canvas"), // 11 Rubble4
-            d.createElement("canvas"), // 12 Jet
-            d.createElement("canvas")  // 13 Sky
+            d.createElement("canvas"), // 2  Smoke
+            d.createElement("canvas"), // 3  Smoke
         ];
 
         this.ctx = [];
@@ -87,7 +62,7 @@ class Banner {
         this.resize();
 
         this.el.append(this.canvas[0]);
-        this.el.removeClass("is-loading");
+        this.el.addClass("is-loaded");
 
         //Show Layers for debug
         /*this.canvas.forEach((canvas,i)=>{
@@ -116,13 +91,11 @@ class Banner {
             this.particles[1] = this.populateSmoke(50);
             this.particles[2] = this.populateFire();
 
-            this.ctx[4].globalAlpha = 0.1;
-            this.ctx[4].globalCompositeOperation = "screen";
-            this.ctx[8].globalAlpha = 0.1;
-            this.ctx[8].globalCompositeOperation = "screen";
+            this.ctx[2].globalAlpha = 0.1;
+            this.ctx[2].globalCompositeOperation = "screen";
+            this.ctx[3].globalAlpha = 0.1;
+            this.ctx[3].globalCompositeOperation = "screen";
             this.ctx[1].filter = "blur(4px)";
-
-            this.drawStatics();
 
         }
 
@@ -135,14 +108,14 @@ class Banner {
 
         if( this.resized ) this.resize();
 
-        this.ctx[8].clearRect(0,0,this.width,this.height);
+        this.ctx[3].clearRect(0,0,this.width,this.height);
         this.particles[0].forEach(particle=>{
-            particle.update( this.ctx[8], this.width, this.height );
+            particle.update( this.ctx[3], this.width, this.height );
         });
 
-        this.ctx[4].clearRect(0,0,this.width,this.height);
+        this.ctx[2].clearRect(0,0,this.width,this.height);
         this.particles[1].forEach(particle=>{
-            particle.update( this.ctx[4], this.width, this.height );
+            particle.update( this.ctx[2], this.width, this.height );
         });
 
         this.ctx[1].clearRect(0,0,this.width,this.height);
@@ -150,85 +123,14 @@ class Banner {
             particle.update( this.ctx[1], this.width, this.height );
         });
 
-
-
-        let top = w.scrollY;
-        let offset = this.el.offset().top;
-        if( offset > top ) top = 0;
-        else top -= offset;
-
         this.ctx[0].clearRect(0,0,this.width,this.height);
-        this.ctx[0].drawImage( this.canvas[13], 0, 0 + (top/1), this.width, this.height );
-        this.ctx[0].drawImage( this.canvas[12], 0, 0 + (top/1.1), this.width, this.height );
-        this.ctx[0].drawImage( this.canvas[11], 0, 0 + (top/1.2), this.width, this.height );
-        this.ctx[0].drawImage( this.canvas[10], 0, 0 + (top/1.4), this.width, this.height );
-        this.ctx[0].drawImage( this.canvas[9],  0, 0 + (top/1.6), this.width, this.height );
-        this.ctx[0].drawImage( this.canvas[8],  0, 0 + (top/2), this.width, this.height );
-        this.ctx[0].drawImage( this.canvas[7],  0, 0 + (top/3), this.width, this.height );
+        this.ctx[0].drawImage( this.canvas[3], 0, 0, this.width, this.height );
+        this.ctx[0].drawImage( this.canvas[2], 0, 0, this.width, this.height );
         this.ctx.globalCompositeOperation = "screen";
-        this.ctx[0].drawImage( this.canvas[6],  0, 0 + (top/4), this.width, this.height );
-        this.ctx.globalCompositeOperation = "source-over";
-        this.ctx[0].drawImage( this.canvas[5],  0, 0 + (top/5), this.width, this.height );
-        this.ctx[0].drawImage( this.canvas[4],  0, 0 + (top/6), this.width, this.height );
-        this.ctx.globalCompositeOperation = "screen";
-        this.ctx[0].drawImage( this.canvas[3],  0, 0 + (top/8), this.width, this.height );
-        this.ctx.globalCompositeOperation = "source-over";
-        this.ctx[0].drawImage( this.canvas[2],  0, 0 + (top/10), this.width, this.height );
-        this.ctx.globalCompositeOperation = "screen";
-        this.ctx[0].drawImage( this.canvas[1],  0, 0 + (top/30), this.width, this.height );
+        this.ctx[0].drawImage( this.canvas[1], 0, 0, this.width, this.height );
         this.ctx.globalCompositeOperation = "source-over";
 
         requestAnimationFrame(this.loop.bind(this));
-    }
-
-
-
-
-
-    //Draws all of the canvas contents that will not change between frames
-    drawStatics(){
-
-        if( this.width > 2000 ) this.drawImg( this.ctx[13], this.images.sky2, "center" );
-        else if( this.width > 800) this.drawImg( this.ctx[13], this.images.sky, "center" );
-        else this.drawImg( this.ctx[13], this.images.sky, "center", 300 );
-
-        if( this.width > 1900 ) this.drawImg( this.ctx[12], this.images.jet, "left", 120, 320 );
-
-        this.drawImg( this.ctx[11], this.images.rubble4, "center", 0, 80 );
-        if( this.width > 1800 ){
-            this.drawImg( this.ctx[11], this.images.rubble4L, "left", 0, 40 );
-            this.drawImg( this.ctx[11], this.images.rubble4R, "right", 0, 40 );
-        }
-
-        this.drawImg( this.ctx[10], this.images.rubble3, "center" );
-        if( this.width > 1000 ){
-            this.drawImg( this.ctx[10], this.images.rubble3, "center", 900 );
-            this.drawImg( this.ctx[10], this.images.rubble3, "center", -900 );
-        }
-
-        if( this.width > 820 ) this.drawImg( this.ctx[9], this.images.rubble2L, "left" );
-        if( this.width > 950 ) this.drawImg( this.ctx[9], this.images.rubble2R, "right" );
-
-        if( this.width > 1400 ) this.drawImg( this.ctx[7], this.images.strange, "center", -680, 60 );
-        if( this.width > 820 ) this.drawImg( this.ctx[7], this.images.vision, "center", 330, 80 );
-        if( this.width > 1750 ) this.drawImg( this.ctx[7], this.images.hulk, "center", 800, 30 );
-
-        this.drawImg( this.ctx[6], this.images.rubble1, "center" );
-        if( this.width > 2000 ){
-            this.drawImg( this.ctx[6], this.images.rubble1, "center", -1900 );
-            this.drawImg( this.ctx[6], this.images.rubble1, "center", 1900 );
-        }
-
-        if( this.width > 1200 ) this.drawImg( this.ctx[5], this.images.groot, "center", 540 );
-        if( this.width > 950 ) this.drawImg( this.ctx[5], this.images.panther, "center", -400 );
-
-        if( this.width > 400 ){
-            this.drawImg( this.ctx[3], this.images.thor, "center", -190 );
-            this.drawImg( this.ctx[3], this.images.captain, "center", 200 );
-        }
-
-        this.drawImg( this.ctx[2], this.images.ironMan, "center" );
-
     }
 
 
@@ -275,7 +177,7 @@ class Banner {
         if( !_.isNumber(height) ) height = 0;
         if( !_.isNumber(speed) ) speed = 1;
         let array = [];
-        let count = this.width / 6;
+        let count = this.width / 20;
 
         for( let i = 0 ; i < count ; i++ ){
 
@@ -313,15 +215,15 @@ class Banner {
                     y: this.height + (Math.random() * this.height)
                 },
                 vel: {
-                    x: (Math.random() * 2) + 1,
-                    y: ((Math.random() * 1) + 0.5) * -1
+                    x: (Math.random() * 0.3) + 0.2,
+                    y: ((Math.random() * 0.5) + 0.4) * -1
                 },
-                size: (Math.random() * 2) + 1,
+                size: (Math.random() * 2) + 2,
                 color: [
-                    Math.random() * 60,
+                    (Math.random() * 40) + 20,
                     (Math.random() * 30) + 70,
-                    (Math.random() * 20) + 60,
-                    (Math.random() * 0.5) + 0.5
+                    (Math.random() * 20) + 40,
+                    (Math.random() * 0.2) + 0.8
                 ]
             };
 
