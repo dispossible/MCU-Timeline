d8fcaf4a246cf99f1157a0feba45940cfa380da5
Timothy Bailey <mail@dispossible.com>
2020-12-17 22:24:55 +0000

    Renamed modal gtag event again

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -43,7 +43,7 @@ export default class Modal {
 
     open(){
         this.el.classList.add("is-open");
-        gtag("event", "Open modal", { modalId: this.id });
+        gtag("event", `Open modal - ${this.id}`);
     }
 
     close(){
